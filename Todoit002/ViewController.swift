214bae7ab23a093a84e695b36707b3a98f6a82b2
Wei Hang Chen <x26006869@icloud.com>
2018-12-10 22:43:25 +0000

    Initial Commit

---
+++ Todoit002/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoit002
+//
+//  Created by chen wei hang on 2018/12/10.
+//  Copyright © 2018 U coffee 001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
